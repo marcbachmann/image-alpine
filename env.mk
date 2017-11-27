75e9c567de8df71f19692f4db239c148074d33a0
Marc Bachmann <marc.brookman@gmail.com>
2017-11-27 02:21:26 +0100

    Move overlay-builder files into correct directory

--- env.mk
+++ env.mk
@@ -1,8 +1,8 @@
-ARCH =					x86_64
-IMAGE_SOURCE_URL =		https://github.com/scaleway/image-alpine
-IMAGE_NAME =			alpine-3.6
-IMAGE_VERSION =			latest
+ARCH =									x86_64
+IMAGE_SOURCE_URL =			https://github.com/scaleway/image-alpine
+IMAGE_NAME =						alpine-3.6
+IMAGE_VERSION =					latest
 IMAGE_VERSION_ALIASES =	3.6.0 3.6 3 edge
-IMAGE_BOOTSCRIPT =		docker
-IMAGE_TITLE =			Alpine Linux 3.6
-IMAGE_DESCRIPTION =		Alpine Linux
+IMAGE_BOOTSCRIPT =			docker
+IMAGE_TITLE =						Alpine Linux 3.6
+IMAGE_DESCRIPTION =			Alpine Linux
